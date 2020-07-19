0ff957e3256ab7cdc235c6dbb98ca33547699d61
konstador <53664849+konstador@users.noreply.github.com>
2020-07-19 14:16:49 +0300

    19/07/20

--- Lotos/app/js/sort.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	 		$('.variables_contents li').click(function(){
-	 		   var get_id = this.id;
-	 		   var get_current = $('.' + 'get_id');
-	 		   $('.discription_class div').not('get_current').hide(200);
-	 		   $('get_current').show(200);
-
-	 		});
-	 	});
--- Lotos/js/slider.js
+++ Lotos/js/slider.js
@@ -1,41 +1,96 @@
 
-    $(document).ready(function(){
-    	$(window).resize(function() {
-    	if ( $(window).width() <821 ){
-	    $('.discription_class').slick({
-	        slidesToShow: 1,
-			  slidesToScroll: 1,
-			  autoplay: false,
-			  autoplaySpeed: 3000,
-			  dots:false,
-			  arrows:true,
-			  nextArrow: $('.next-btn'),
-			  prevArrow: $('.prev-btn'),
-			  
-	      });
-	      $('.discription_treiner').slick({
-	        slidesToShow: 1,
-			  slidesToScroll: 1,
-			  autoplay: false,
-			  autoplaySpeed: 3000,
-			  dots:false,
-			  arrows:true,
-			  nextArrow: $('.treiners_next-btn'),
-			  prevArrow: $('.treiners_prev-btn'),
-			  
-	      });
-	      $('.discription_review').slick({
-	        slidesToShow: 1,
-			  slidesToScroll: 1,
-			  autoplay: false,
-			  autoplaySpeed: 3000,
-			  dots:false,
-			  arrows:true,
-			  nextArrow: $('.review_next-btn'),
-			  prevArrow: $('.review_prev-btn'),
-			  
-	      });
-	   };
-	 });
-    });
-  
+   $(document).ready(function () {
+   	$(window).resize(function () {
+   		if ($(window).width() < 821) {
+   			$('.discription_class').slick({
+   				slidesToShow: 1,
+   				slidesToScroll: 1,
+   				autoplay: false,
+   				autoplaySpeed: 3000,
+   				dots: false,
+   				arrows: true,
+   				nextArrow: $('.next-btn'),
+   				prevArrow: $('.prev-btn'),
+   			});
+   			$('.discription_treiner').slick({
+   				slidesToShow: 1,
+   				slidesToScroll: 1,
+   				autoplay: false,
+   				autoplaySpeed: 3000,
+   				dots: false,
+   				arrows: true,
+   				nextArrow: $('.treiners_next-btn'),
+   				prevArrow: $('.treiners_prev-btn'),
+
+   			});
+   			$('.discription_review').slick({
+   				slidesToShow: 1,
+   				slidesToScroll: 1,
+   				autoplay: false,
+   				autoplaySpeed: 3000,
+   				dots: false,
+   				arrows: true,
+   				nextArrow: $('.review_next-btn'),
+   				prevArrow: $('.review_prev-btn'),
+
+   			});
+   		} else {
+   			$('.discription_class').slick('unslick');
+   			$('.discription_treiner').slick('unslick');
+   			$('.discription_review').slick('unslick');
+   		}
+   	});
+   });
+   $(document).ready(function () {
+					$('.discription_class').slick({
+						mobileFirst:true,
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: false,
+						autoplaySpeed: 3000,
+						dots: false,
+						arrows: true,
+						nextArrow: $('.next-btn'),
+						prevArrow: $('.prev-btn'),
+						responsive: [
+						{
+						breakpoint: 821,
+						settings: "unslick",
+						}]
+					});
+					$('.discription_treiner').slick({
+						mobileFirst:true,
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: false,
+						autoplaySpeed: 3000,
+						dots: false,
+						arrows: true,
+						nextArrow: $('.treiners_next-btn'),
+						prevArrow: $('.treiners_prev-btn'),
+						responsive: [
+						{
+						breakpoint: 821,
+						settings: "unslick",
+						}]
+
+					});
+					$('.discription_review').slick({
+						mobileFirst:true,
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						autoplay: false,
+						autoplaySpeed: 3000,
+						dots: false,
+						arrows: true,
+						nextArrow: $('.review_next-btn'),
+						prevArrow: $('.review_prev-btn'),
+						responsive: [
+						{
+						breakpoint: 821,
+						settings: "unslick",
+						}]
+
+					});
+				
+		});
